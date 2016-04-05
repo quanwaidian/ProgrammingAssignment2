64dd6ede8d654c72bb6556ca6d6cca46e51346d8
quanwaidian <quanwaidian@163.com>
2016-04-05 15:52:23 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,33 +1,22 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
 makeCacheMatrix <- function(x = matrix()) {
-        a<-NULL
-        set<-function(y){
-                x<<-y
-                m<<-NULL
-        }
-        get<-function() x
-        setmatrix<-function(solve) a<<- solve
-        getmatrix<-function() a
-        list(set=set, get=get,
-        setmatrix=setmatrix,
-        getmatrix=getmatrix)
+    a<-NULL
+    set<-function(y){
+        x<<-y
+        m<<-NULL
+    }
+    get<-function() x
+    setsolve<-function(solve) a<<- solve
+    getmatrix<-function() a
+    list(set=set, get=get,setsolve=setsolve,getmatrix=getmatrix)
 }
-
-
-
-
-cacheSolve <- function(x, ...) {
-        a<-x$getmatrix()
-        if(!is.null(a)){
-                message("getting cached data")
-                return(a)
-        }
-        matrix<-x$get
-        a<-solve(matrix, ...)
-        x$setmatrix(a)
-        a
+cacheSolve <- function(x=matrix(), ...) {
+    a<-x$getmatrix()
+    if(!is.null(a)){
+        message("getting cached data")
+        return(a)
+    }
+    matrix<-x$get
+    a<-solve(matrix, ...)
+    x$setsolve(a)
+    a
 }
